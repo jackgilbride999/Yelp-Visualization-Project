63e3e711e15fac804ff29c535767a85a18e6bf4a
owjohnst <owjohnst@c6451956-8462-4b62-a6f6-882f49c0bdc5>
2018-03-28 12:05:20 +0000

    fix to business screen text

--- ProgrammingProjectIntelliJ/src/main/java/com/LukeHackett/Main.java
+++ ProgrammingProjectIntelliJ/src/main/java/com/LukeHackett/Main.java
@@ -149,7 +149,7 @@ public class Main extends PApplet {
         spaceFromEdge = " ";
 
         textSize(18);
-        while (textWidth(spaceFromEdge) < 180) {
+        while (textWidth(spaceFromEdge) < 192) {
             spaceFromEdge += " ";
         }
 
