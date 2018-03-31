a957c70eb6505367dae06e52477b9372952f85c8
lhackett <lhackett@c6451956-8462-4b62-a6f6-882f49c0bdc5>
2018-03-31 02:43:50 +0000

    Added redundancy for image checking

--- ProgrammingProjectIntelliJ/src/main/java/com/LukeHackett/ImageCrawler.java
+++ ProgrammingProjectIntelliJ/src/main/java/com/LukeHackett/ImageCrawler.java
@@ -1,10 +1,18 @@
 package com.LukeHackett;
 
+import com.microsoft.rest.credentials.ServiceClientCredentials;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import processing.core.PApplet;
 import processing.core.PImage;
 import com.microsoft.azure.cognitiveservices.imagesearch.ImageObject;
 import com.microsoft.azure.cognitiveservices.imagesearch.implementation.ImageSearchAPIImpl;
 import com.microsoft.azure.cognitiveservices.imagesearch.implementation.ImagesInner;
+import processing.data.XML;
+
+import java.io.IOException;
 
 
 public class ImageCrawler extends Thread {
@@ -24,39 +32,55 @@ public class ImageCrawler extends Thread {
     }
 
     PImage findPhoto(Business b) {
-        String urlPhoto = imageSearch("86b3dc772e3b470e8b100d151a0e0da2", "3f01e0d67a13451c80d265d8e5b3d55d", b);
-        return canvas.loadImage(urlPhoto);
+        String urlPhotoBing = imageSearch("86b3dc772e3b470e8b100d151a0e0da2", "3f01e0d67a13451c80d265d8e5b3d55d", b);
+        if (urlPhotoBing != null) {
+            System.out.println("Binged it!");
+            return canvas.loadImage(urlPhotoBing);
+        } else {
+            String urlPhotoGoogle = googleImageAttempt(b);
+            System.out.println("Googled it!");
+            return canvas.loadImage(urlPhotoGoogle, "jpg");
+        }
     }
 
     public Business getBusiness() {
         return business;
     }
 
-    public static String imageSearch(String subscriptionKey, String subscriptionKeyAlt, Business b) {
+    public ImageSearchAPIImpl getClient(final String subscriptionKey) {
+        return new ImageSearchAPIImpl("https://api.cognitive.microsoft.com/bing/v7.0/",
+                new ServiceClientCredentials() {
+                    public void applyCredentialsFilter(OkHttpClient.Builder builder) {
+                        builder.addNetworkInterceptor(
+                                new Interceptor() {
+                                    public Response intercept(Chain chain) throws IOException {
+                                        Request request = null;
+                                        Request original = chain.request();
+                                        // Request customization: add request headers
+                                        Request.Builder requestBuilder = original.newBuilder()
+                                                .addHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+                                        request = requestBuilder.build();
+                                        return chain.proceed(request);
+                                    }
+                                });
+                    }
+                });
+    }
+
+    public String imageSearch(String subscriptionKey, String subscriptionKeyAlt, Business b) {
+        //TODO Add category searching if you cant find a business image
+
         String subKeyToUse = subscriptionKey;
         boolean done = false;
         while (!done) {
-            ImageSearchAPIImpl client = Experiment.getClient(subKeyToUse);
+            ImageSearchAPIImpl client = getClient(subKeyToUse);
             try {
-                ImagesInner imageResults = client.searchs().list(b.getName() + " yelp");
-                System.out.println("\r\nSearch images for query " + b.getName());
+                ImagesInner imageResults = client.searchs().list(b.getName() + " " + "yelp");
 
-                if (imageResults == null) {
-                    System.out.println("No image result data.");
-                } else {
-                    // Image results
-                    if (imageResults.value().size() > 0) {
-                        ImageObject firstImageResult = imageResults.value().get(0);
+                if (imageResults != null && imageResults.value().size() > 0) {
+                    ImageObject firstImageResult = imageResults.value().get(0);
+                    return firstImageResult.contentUrl();
 
-                        System.out.println(String.format("Image result count: %d", imageResults.value().size()));
-                        System.out.println(String.format("First image insights token: %s", firstImageResult.imageInsightsToken()));
-                        System.out.println(String.format("First image thumbnail url: %s", firstImageResult.thumbnailUrl()));
-                        System.out.println(String.format("First image content url: %s", firstImageResult.contentUrl()));
-
-                        return firstImageResult.contentUrl();
-                    } else {
-                        System.out.println("Couldn't find image results!");
-                    }
                 }
             } catch (Exception e) {
                 if (subKeyToUse.equals(subscriptionKeyAlt)) {
@@ -69,6 +93,29 @@ public class ImageCrawler extends Thread {
                 done = true;
             }
         }
-        return "businessPlaceholder.png";
+        return null;
+    }
+
+    public String googleImageAttempt(Business b) {
+        String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/xml?"
+                + "location=" + b.getLatitude() + ',' + b.getLongitude()
+                + "&name=" + b.getName().replaceAll(" ", "%20").replaceAll("'", "")
+                + "&radius=500&key=AIzaSyAdNw5lE_KJe9uhRRb2fKi2U8Ex63HfYL8";
+
+        //Make XML object and parse photo reference and load the image
+        XML xmlPlace = canvas.loadXML(url);
+        String urlPhoto = "businessPlaceholder.png";
+        if (xmlPlace.getChild("status").getContent().equals("OK")) {
+            String photoReference;
+            try {
+                if ((photoReference = xmlPlace.getChild("result").getChild("photo").getChild("photo_reference").getContent()) != null)
+                    urlPhoto = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=180&maxheight=180&photoreference=" + photoReference + "&key=AIzaSyAdNw5lE_KJe9uhRRb2fKi2U8Ex63HfYL8";
+            } catch (Exception e) {
+                urlPhoto = xmlPlace.getChild("result").getChild("icon").getContent();
+            }
+            return urlPhoto;
+        } else {
+            return urlPhoto;
+        }
     }
 }
